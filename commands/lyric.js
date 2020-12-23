6456a3c2a468b11925bed3b0fa04c7dc4c6ace4e
MichaelKowulich <mkowu1@brockport.edu>
2020-12-23 15:46:03 -0500

    Database Changes, lyric functionality (EPS only)

---
+++ commands/lyric.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: 'lyric',
+    description: "gets random 1975 lyric",
+    execute(message, args, database){
+        var max = 220;
+        var min = 1;
+        var rand = Math.floor(Math.random() * (max - min) + min);
+        let query = 'SELECT description FROM lyric WHERE id = '.concat(rand.toString());
+        database.get(query, (err, row) => {
+            if (err){
+                console.log(err);
+                return;
+            }
+            if(row != undefined) {
+                let lyric = row.description;
+                message.channel.send(lyric);
+            }
+        });
+    }
+}
