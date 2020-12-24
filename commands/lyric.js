2da001ef1af7c36b5a2c87ecfa094a653016ba7b
MichaelKowulich <mkowu1@brockport.edu>
2020-12-24 15:08:35 -0500

    lyric command up until noacf

--- commands/lyric.js
+++ commands/lyric.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "lyric",
   description: "gets random 1975 lyric",
   execute(message, args, database) {
-    var max = 568;
+    var max = 971;
     var min = 1;
     var rand = Math.floor(Math.random() * (max - min) + min);
     let query = "SELECT description FROM lyric WHERE id = ".concat(
